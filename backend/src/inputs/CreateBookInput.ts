a32e7805f5d7490cc2aaabdfa267c9c1770beb3b
nara-ryoya <nara_ryoya@icloud.com>
2023-03-12 17:42:24 +0900

    feat: setup resolver

---
+++ backend/src/inputs/CreateBookInput.ts
@@ -0,0 +1,13 @@
+import { InputType, Field } from "type-graphql";
+
+@InputType()
+export class CreateBookInput {
+  @Field()
+  title: string;
+
+  @Field()
+  author: string;
+
+  @Field({nullable: true})
+  isPublished?: boolean;
+}
